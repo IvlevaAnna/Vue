c1afbfd5dfec27aea6b1b7a211250a2384887071
IvlevaAnna <annaiva19@mail.ru>
2021-12-16 00:56:19 +0300

    Calculator

---
+++ fp/src/components/Calculator.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <div class="display">
+      <input v-model.number="operand1" type="number"/>
+      <input v-model.number="operand2" type="number"/>
+      = {{result}}
+    </div>
+    <div class="keyboard">
+      <button @click="sum">+</button>
+      <button @click="sub">-</button>
+      <button @click="divide">/</button>
+      <button @click="mult">*</button>
+      <button @click="pow">^</button>
+      <button @click="intDivide">//</button>
+    </div>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: "Calculator",
+  data () {
+    return {
+      operand1: 0,
+      operand2: 0,
+      result: 0
+    }
+  },
+  methods: {
+    sum () {
+      this.result = this.operand1 + this.operand2
+    },
+    sub () {
+      this.result = this.operand1 - this.operand2
+    },
+    divide () {
+      this.result = this.operand1 / this.operand2
+    },
+    mult () {
+      this.result = this.operand1 * this.operand2
+    },
+    pow () {
+      this.result = Math.pow(this.operand1, this.operand2)
+    },
+    intDivide () {
+      this.result = Math.floor(this.operand1 / this.operand2)
+    }
+  }
+}
+</script>
